a0053b40ce4d7bbac54bfa5f0e36e0f863f9db53
Mark Spratt <panzerlink@gmail.com>
2024-04-21 11:34:15 -0500

    Add more quircky status messages

--- C#/APP/Find Project/Utilities/AppOperations.cs
+++ C#/APP/Find Project/Utilities/AppOperations.cs
@@ -30,11 +30,9 @@ namespace Find_Project.Utilities
             }
         }
 
-
         // Create a list of status messages to display in the status bar
         public string RandomStatusMessage()
         {
-
             List<string> statusMessages = new()
             {
                 "Ready for a file-finding adventure?",
@@ -84,7 +82,57 @@ namespace Find_Project.Utilities
                 "Embark on a wild ride through your directories!",
                 "Crack open those virtual vaults!",
                 "Witness the magic of organization!",
-                "Activate the file-finding frenzy!"
+                "Activate the file-finding frenzy!",
+                "Let's dig into your digital treasure trove!",
+                "Ready to wrangle those wandering files?",
+                "Cue the drumroll! Let's find those files!",
+                "Prepare to conquer your digital chaos!",
+                "Time to bring order to the digital mayhem!",
+                "Let's turn your file jungle into a file paradise!",
+                "On your mark, get set, organize!",
+                "Grab your virtual flashlight! We're exploring your files!",
+                "Let's navigate the labyrinth of your folders!",
+                "Blast off into the realm of organized files!",
+                "Unveil the hidden gems buried in your folders!",
+                "Prepare for a digital scavenger hunt!",
+                "Ready to untangle the web of files?",
+                "Let's be the heroes your files deserve!",
+                "Welcome to the land of file-finding wonders!",
+                "Brace yourself for a file-finding odyssey!",
+                "Let's sprinkle some organization magic!",
+                "Get your file-finding engines revved!",
+                "Onward, to the land of organized bliss!",
+                "Charting a course through your file galaxy!",
+                "Let's embark on a journey to file serenity!",
+                "Dive into the sea of your digital documents!",
+                "Let's give your files the spotlight they deserve!",
+                "Buckle up for a rollercoaster ride through your files!",
+                "Ready to bring order to the file chaos?",
+                "Prepare for file-finding greatness!",
+                "Let's dust off those neglected files and give them a home!",
+                "File-finding mode: activated!",
+                "Let's make file-finding an art form!",
+                "Your files called—they're ready to be found!",
+                "Navigate the file maze with finesse!",
+                "We're on a mission to rescue your lost files!",
+                "Time to shine a light on your file darkness!",
+                "Let's crack the code of your file organization!",
+                "Embrace the thrill of the file hunt!",
+                "Prepare to witness the magic of file-finding!",
+                "Welcome to the file-finding party!",
+                "Your files are waiting for their moment in the sun!",
+                "Let's tame the file chaos like a digital cowboy!",
+                "Clear the clutter, find the files!",
+                "We're the file-finding dream team!",
+                "Let's turn your file frown upside down!",
+                "Ready to decode the language of your files?",
+                "Let's play matchmaker with your files and folders!",
+                "Unleash the power of your organized files!",
+                "Let's uncover the buried treasures in your folders!",
+                "Your files are our top priority!",
+                "We're on a quest for file-finding glory!",
+                "Prepare for a file-finding revolution!",
+                "Let's make your file dreams a reality!",
             };
             Random random = new();
             int index = random.Next(statusMessages.Count);
