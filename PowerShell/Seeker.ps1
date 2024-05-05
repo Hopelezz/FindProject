e947f5dbb21b781d721308eb7d3174d9ea4e78e4
Mark Spratt <panzerlink@gmail.com>
2024-05-05 14:03:24 -0500

    Fix search based on extension type

--- PowerShell/Seeker.ps1
+++ PowerShell/Seeker.ps1
@@ -6,28 +6,31 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+$font = New-Object System.Drawing.Font("Microsoft Sans Serif", 10, [System.Drawing.FontStyle]::Regular)
+
 # Create the main form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Seeker"
-$form.Size = New-Object System.Drawing.Size(700, 500)
+$form.Size = New-Object System.Drawing.Size(600, 350)
 $form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
 $icon = [System.Drawing.Icon]::ExtractAssociatedIcon($PSHOME + "\powershell.exe")
 $form.Icon = $icon
+# Create padding on form
+$form.Padding = New-Object System.Windows.Forms.Padding(5)
 
-    
 # Create a grid layout
 $layout = New-Object System.Windows.Forms.TableLayoutPanel
     $layout.Dock = [System.Windows.Forms.DockStyle]::Fill
     $layout.ColumnCount = 1
     $layout.RowCount = 2
-    $layout.RowStyles.Add((New-Object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Percent, 100)))
-    $layout.RowStyles.Add((New-Object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Absolute, 30)))
+    $layout.RowStyles.Add((New-Object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Percent, 100))) | Out-Null
+    $layout.RowStyles.Add((New-Object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Absolute, 30))) | Out-Null
 
     # Create a status bar
 $statusBar = New-Object System.Windows.Forms.StatusStrip
     $statusLabel = New-Object System.Windows.Forms.ToolStripStatusLabel
     $statusLabel.Text = "All systems go!"
-    $statusBar.Items.Add($statusLabel)
+    $statusBar.Items.Add($statusLabel) | Out-Null
 
 # Create a tab control
 $tabControl = New-Object System.Windows.Forms.TabControl
@@ -38,37 +41,31 @@ $tabControl = New-Object System.Windows.Forms.TabControl
 # Create the Search tab
 $searchTab = New-Object System.Windows.Forms.TabPage
     $searchTab.Text = "Search"
-    $searchTab.Padding = New-Object System.Windows.Forms.Padding(10)
 
     # Create controls for search tab
     $searchTextBox = New-Object System.Windows.Forms.TextBox
     $searchTextBox.Location = New-Object System.Drawing.Point(10, 15)
     $searchTextBox.Size = New-Object System.Drawing.Size(400, 25)
-    # Increate the font size for the search text box
-    $searchTextBox.Font = New-Object System.Drawing.Font("Microsoft Sans Serif", 12, [System.Drawing.FontStyle]::Regular)
+    $searchTextBox.Font = $font
 
     $searchButton = New-Object System.Windows.Forms.Button
     $searchButton.Location = New-Object System.Drawing.Point(420, 15)
     $searchButton.Size = New-Object System.Drawing.Size(80, 25)
     $searchButton.Text = "Search"
 
-    $warningLabel = New-Object System.Windows.Forms.Label
-    $warningLabel.Location = New-Object System.Drawing.Point(10, 45)
-    $warningLabel.Size = New-Object System.Drawing.Size(300, 25)
-    $warningLabel.Visible = $false
-    $warningLabel.Text = "Input must contain at least 3 characters."
-    $WarningLabel.ForeColor = 'Red'
-
     $resultsListBox = New-Object System.Windows.Forms.ListBox
-    $resultsListBox.Location = New-Object System.Drawing.Point(10, 80)
+    $resultsListBox.Location = New-Object System.Drawing.Point(10, 45)
     $resultsListBox.Size = New-Object System.Drawing.Size(660, 220)
+    $resultsListBox.Width = $searchTab.Width - 20
+    $resultsListBox.Height = $searchTab.Height - 45
+    
     $resultsListBox.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor 
     [System.Windows.Forms.AnchorStyles]::Bottom -bor 
     [System.Windows.Forms.AnchorStyles]::Left -bor 
     [System.Windows.Forms.AnchorStyles]::Right
-    $resultsListBox.Font = New-Object System.Drawing.Font("Microsoft Sans Serif", 10, [System.Drawing.FontStyle]::Regular)
+    $resultsListBox.Font = $font
 
-$searchTab.Controls.AddRange(@($searchTextBox, $searchButton, $warningLabel, $resultsListBox))
+$searchTab.Controls.AddRange(@($searchTextBox, $searchButton, $resultsListBox))
 
 # [=========== Settings Tab ===========]
 
@@ -86,11 +83,12 @@ $settingsTab.Padding = New-Object System.Windows.Forms.Padding(10)
     $defaultPathTextBox = New-Object System.Windows.Forms.TextBox
     $defaultPathTextBox.Size = New-Object System.Drawing.Size(300, 25)
     $defaultPathTextBox.Location = New-Object System.Drawing.Point(110, 10)
+    $defaultPathTextBox.Font = $font
 
     $defaultPathButton = New-Object System.Windows.Forms.Button
-    $defaultPathButton.Text = "Browse"
-    $defaultPathButton.Size = New-Object System.Drawing.Size(80, 25)
-    $defaultPathButton.Location = New-Object System.Drawing.Point(420, 10)
+    $defaultPathButton.Text = "..."
+    $defaultPathButton.Size = New-Object System.Drawing.Size(30, 25)
+    $defaultPathButton.Location = New-Object System.Drawing.Point(520, 10)
     $defaultPathButton.Add_Click({
         $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
         $folderBrowser.Description = "Select default path"
@@ -109,11 +107,12 @@ $settingsTab.Padding = New-Object System.Windows.Forms.Padding(10)
     $ctrlPathTextBox = New-Object System.Windows.Forms.TextBox
     $ctrlPathTextBox.Size = New-Object System.Drawing.Size(300, 25)
     $ctrlPathTextBox.Location = New-Object System.Drawing.Point(110, 40)
+    $ctrlPathTextBox.Font = $font
 
     $ctrlPathButton = New-Object System.Windows.Forms.Button
-    $ctrlPathButton.Text = "Browse"
-    $ctrlPathButton.Size = New-Object System.Drawing.Size(80, 25)
-    $ctrlPathButton.Location = New-Object System.Drawing.Point(420, 40)
+    $ctrlPathButton.Text = "..."
+    $ctrlPathButton.Size = New-Object System.Drawing.Size(30, 25)
+    $ctrlPathButton.Location = New-Object System.Drawing.Point(520, 40)
     $ctrlPathButton.Add_Click({
         $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
         $folderBrowser.Description = "Select ctrl path"
@@ -132,11 +131,12 @@ $settingsTab.Padding = New-Object System.Windows.Forms.Padding(10)
     $shiftPathTextBox = New-Object System.Windows.Forms.TextBox
     $shiftPathTextBox.Size = New-Object System.Drawing.Size(300, 25)
     $shiftPathTextBox.Location = New-Object System.Drawing.Point(110, 70)
+    $shiftPathTextBox.Font = $font
 
     $shiftPathButton = New-Object System.Windows.Forms.Button
-    $shiftPathButton.Text = "Browse"
-    $shiftPathButton.Size = New-Object System.Drawing.Size(80, 25)
-    $shiftPathButton.Location = New-Object System.Drawing.Point(420, 70)
+    $shiftPathButton.Text = "..."
+    $shiftPathButton.Size = New-Object System.Drawing.Size(30, 25)
+    $shiftPathButton.Location = New-Object System.Drawing.Point(520, 70)
     $shiftPathButton.Add_Click({
         $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
         $folderBrowser.Description = "Select shift path"
@@ -150,15 +150,19 @@ $settingsTab.Padding = New-Object System.Windows.Forms.Padding(10)
     $searchTypeComboBox.Size = New-Object System.Drawing.Size(150, 25)
     $searchTypeComboBox.Location = New-Object System.Drawing.Point(10, 100)
     $searchTypeComboBox.Items.AddRange(@("Folder", "File"))
+    # If folder is not selected clear file Type
+    $searchTypeComboBox.Add_SelectedIndexChanged({
+        if ($searchTypeComboBox.SelectedItem -eq "Folder") {
+            $fileTypeComboBox.SelectedIndex = -1
+        }
+    })
     $searchTypeComboBox.Add_SelectedIndexChanged({
         if ($searchTypeComboBox.SelectedItem -eq "File") {
-            $ctrlPathTextBox.Enabled = $false
-            $shiftPathTextBox.Enabled = $false
             $fileTypeComboBox.Enabled = $true
+            # default to All files
+            $fileTypeComboBox.SelectedIndex = 0
         }
         else {
-            $ctrlPathTextBox.Enabled = $true
-            $shiftPathTextBox.Enabled = $true
             $fileTypeComboBox.Enabled = $false
         }
     })
@@ -180,7 +184,7 @@ $settingsTab.Padding = New-Object System.Windows.Forms.Padding(10)
         "Code" = ".py", ".js", ".html", ".css", ".cpp", ".java", ".c", ".cs", ".php", ".sql", ".xml", ".json", ".ps1"
         "Archives" = ".zip", ".rar", ".7z", ".tar", ".gz", ".bz2"
         "Executable files" = ".exe", ".msi", ".bat", ".sh"
-        "Fonts" = ".ttf", ".otf", ".fon"
+        "Fonts" = ".ttf", ".otf", ".fon", ".woff", ".woff2"
         "CAD files" = ".dwg", ".dxf"
         "GIS files" = ".shp", ".kml", ".kmz"
         "Backup files" = ".bak", ".backup", ".zip", ".tar.gz"
@@ -194,13 +198,28 @@ $settingsTab.Padding = New-Object System.Windows.Forms.Padding(10)
         "Disk images" = ".iso", ".img"
         "Encrypted files" = ".pgp", ".asc"
         "Temporary files" = ".tmp", ".temp"
-    }    
-    $fileTypeComboBox.Items.AddRange(@( $fileTypes.Keys))
-    # Update status bar based on the selected file type
+        "Torrent files" = ".torrent"
+        "Comic files" = ".cbr", ".cbz"
+        "Ebook files" = ".epub", ".mobi", ".azw"
+    }
+    $fileTypeComboBox.Items.AddRange(@( $fileTypes.Keys | Sort-Object))
+
+    # Variable to store the previous selected file type to prevent displaying the same message multiple times
+    $previousSelectedFileType = $null
     $fileTypeComboBox.Add_SelectedIndexChanged({
-        $selectedFileType = $fileTypeComboBox.SelectedItem.ToString()
-        $selectedFileExtensions = $fileTypes[$selectedFileType]
-        UpdateStatus("Selected file type: $selectedFileType ($($selectedFileExtensions -join ', '))")
+        $selectedFileType = $fileTypeComboBox.SelectedItem
+        if ($selectedFileType -ne $null) {
+            $selectedFileTypeString = $selectedFileType.ToString()
+            $selectedFileExtensions = $fileTypes[$selectedFileTypeString]
+            
+            # Check if the selected file type is different from the previous one
+            if ($selectedFileTypeString -ne $previousSelectedFileType) {
+                UpdateStatus("Selected file type: $($selectedFileTypeString) ($($selectedFileExtensions -join ', '))")
+                
+                # Update the previous selected file type
+                $previousSelectedFileType = $selectedFileTypeString
+            }
+        }
     })
 
     $saveSettingsButton = New-Object System.Windows.Forms.Button
@@ -233,8 +252,6 @@ $layout.Controls.Add($statusBar)
 # Add layout to the form
 $form.Controls.Add($layout)
 
-
-
 # Function to handle save settings button click
 $saveSettingsButton.Add_Click({
     $settings = @{
@@ -261,35 +278,38 @@ $form.Add_Load({
         $shiftPathTextBox.Text = $settingsJson.ShiftPath
         $searchTypeComboBox.SelectedItem = $settingsJson.SearchType
         $fileTypeComboBox.SelectedItem = $settingsJson.FileType
+        # Clear the file type combo box if search type is Folder
+        if ($settingsJson.SearchType -eq "Folder") {
+            $fileTypeComboBox.SelectedIndex = -1
+        }
         if ($settingsJson.SearchType -eq "File") {
-            $ctrlPathTextBox.Enabled = $false
-            $shiftPathTextBox.Enabled = $false
             $fileTypeComboBox.Enabled = $true
+            
         }
     }
 })
 
 # Function to handle key press events
 $searchTextBox.Add_KeyDown({
-    param($sender, $e)
-    # Check if Ctrl+Enter is pressed
-    if ($e.Control -and $e.KeyCode -eq 'Enter') {
-        # Call Search function with the ctrlPathTextBox.Text
-        Search -path $ctrlPathTextBox.Text
-        UpdateStatus("Path Searched " + $ctrlPathTextBox.Text)
-    }
-    # Check if Shift+Enter is pressed
-    elseif ($e.Shift -and $e.KeyCode -eq 'Enter') {
-        # Call Search function with the shiftPathTextBox.Text
-        Search -path $shiftPathTextBox.Text
-        UpdateStatus("Path Searched " + $shiftPathTextBox.Text)
-    }
-    # Check if Enter is pressed without any modifier keys
-    elseif ($e.KeyCode -eq 'Enter') {
-        # Call Search function with the defaultPathTextBox.Text
-        Search -path $defaultPathTextBox.Text
-        UpdateStatus("Path Searched " + $defaultPathTextBox.Text)        
-    }
+    $e = $args[1]
+        # Check if Ctrl+Enter is pressed
+        if ($e.Control -and $e.KeyCode -eq 'Enter') {
+            # Call Search function with the ctrlPathTextBox.Text
+            Search -path $ctrlPathTextBox.Text 
+            UpdateStatus("Searching path: " + $ctrlPathTextBox.Text)
+        }
+        # Check if Shift+Enter is pressed
+        elseif ($e.Shift -and $e.KeyCode -eq 'Enter') {
+            # Call Search function with the shiftPathTextBox.Text
+            Search -path $shiftPathTextBox.Text
+            UpdateStatus("Searching path: " + $shiftPathTextBox.Text)
+        }
+        # Check if Enter is pressed without any modifier keys
+        elseif ($e.KeyCode -eq 'Enter') {
+            # Call Search function with the defaultPathTextBox.Text
+            Search -path $defaultPathTextBox.Text
+            UpdateStatus("Searching path: " + $defaultPathTextBox.Text)
+        }
 })
 
 $searchButton.Add_Click({
@@ -310,17 +330,26 @@ function Search {
     try {
         $searchText = $searchTextBox.Text
         $searchType = $searchTypeComboBox.SelectedItem.ToString()
-        $fileTypesSelected = $fileTypes[$fileTypeComboBox.SelectedItem.ToString()]
-        $WarningLabel.Visible = $false
+
+        # Check if search type is Folder
+        if ($searchType -eq "Folder") {
+            $fileTypeSelected = $null  # Set fileTypeSelected to null
+        }
+        else {
+            # Check if "All files" is selected
+            if ($fileTypeComboBox.SelectedItem -eq "All files") {
+                $fileTypeSelected = $null  # Don't filter by file type
+            }
+            else {
+                $fileTypeSelected = $fileTypes[$fileTypeComboBox.SelectedItem.ToString()]
+            }
+        }
 
         Write-Host "Search Text: $searchText"
         Write-Host "Search Type: $searchType"
-        Write-Host "Selected File Types Count: $($fileTypesSelected.Count)"
-        Write-Host "Selected File Types: $($fileTypesSelected -join ', ')"
         
         if ($searchText.Length -lt 3) {
-            $WarningLabel.Text = "Input must contain at least 3 characters."
-            $WarningLabel.Visible = $true
+            UpdateStatus("Input must contain at least 3 characters.")
         }
         else {
             if ($searchType -eq "Folder") {
@@ -331,9 +360,17 @@ function Search {
                 }
             }
             elseif ($searchType -eq "File") {
-                Get-ChildItem -Path $path -File -Recurse | Where-Object { $_.Name -like "*$searchText*" -and $fileTypesSelected -contains $_.Extension } | ForEach-Object {
-                    $resultsListBox.Items.Add($_.FullName)
-                    Write-Host $_.FullName
+                if ($fileTypeSelected -eq $null) {
+                    Get-ChildItem -Path $path -File -Recurse | Where-Object { $_.Name -like "*$searchText*" } | ForEach-Object {
+                        $resultsListBox.Items.Add($_.FullName)
+                        Write-Host $_.FullName
+                    }
+                }
+                else {
+                    Get-ChildItem -Path $path -File -Recurse | Where-Object { $_.Name -like "*$searchText*" -and $fileTypeSelected -contains $_.Extension } | ForEach-Object {
+                        $resultsListBox.Items.Add($_.FullName)
+                        Write-Host $_.FullName
+                    }
                 }
             }
         }
